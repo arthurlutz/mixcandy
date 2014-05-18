f73ffc86d37ded48222cda562af5927676541363
francismakes <hello@francismakes.com>
2014-05-18 01:15:08 -0700

    Commit 9-cube shape, color table, message table.

--- grid-francis/js/beatpad.js
+++ grid-francis/js/beatpad.js
@@ -10,6 +10,25 @@ var numRows = 8,
     numCols = 8,
     offset = 200; // Compensates for 200ms lag (LPR:2)
 
+// Message Table
+var message = {
+  off: 0x80,
+  on: 0x90,
+  rapid: 0x92
+};
+
+// Color Table
+var color = {
+  none: 0x00,
+  lGreen: 0x10,
+  mGreen: 0x20,
+  hGreen: 0x30,
+  hYellow: 0x3f,
+  mYellow: 0x2f,
+  lYellow: 0x1f,
+  red: 0x0f
+};
+
 // Instantiate a new Howl instance with a particular `.mp3`.
 var songLocation = '../songs/geo.mp3';
 var song = new Howl({ urls: [songLocation] });
@@ -108,15 +127,16 @@ function onMIDISuccess (access) {
    *
    * @param midiMessage [sp1, sp2, sp3]
    *    => @sp1 messageType {Number (hex)}
-   *    => @sp2 event.data[1] {Number}
+   *    => @sp2 event.data[1] {Number} // Origin: myMIDIMessagehandler
    *    => @sp3 velocity {Number (hex)} // (LPR:3)
    * @param optionalDelay {Number}
    */
 
-  // LaunchPad S will either light up at start or every time browser refreshes.
-  outputs.send([0x80, 0x25, 0x7f], window.performance.now() + 1000);
-  outputs.send([0x90, 0x25, 0x7f]);
+  // LaunchPad S will light amber at start or every time browser refreshes.
+  outputs.send([message.off, 0x25, color.hYellow], window.performance.now() + 1000);
+  outputs.send([message.on, 0x25, color.hYellow]);
 }
+
 
 /**
  * @function - myMidiMessageHandler
@@ -133,13 +153,33 @@ function myMIDIMessagehandler (event) {
 	if (currentVelocity == 0) {
 
 		var currentButton = event.data[1];
+    console.log(currentButton);
 
     // Set the song position accordingly.
 		song.pos(BEATS[offset + currentButton]);
 
-    // Creates a press-and-release effect.
-    outputs.send([0x90, currentButton, 0x30]);
+    // TODO: Refactor this into some configurable function.
+    // This alone will leave the button on.
+    outputs.send([0x90, currentButton, 0x3f]);
+    outputs.send([0x90, currentButton + 15, 0x3f]);
+    outputs.send([0x90, currentButton - 15, 0x3f]);
+    outputs.send([0x90, currentButton + 16, 0x3f]);
+    outputs.send([0x90, currentButton - 16, 0x3f]);
+    outputs.send([0x90, currentButton + 17, 0x3f]);
+    outputs.send([0x90, currentButton - 17, 0x3f]);
+    outputs.send([0x90, currentButton + 1, 0x3f]);
+    outputs.send([0x90, currentButton - 1, 0x3f]);
+
+    // But adding this creates a press-and-release effect.
     outputs.send([0x90, currentButton, 0x00], window.performance.now() + 100);
+    outputs.send([0x90, currentButton + 15, 0x00], window.performance.now() + 100);
+    outputs.send([0x90, currentButton - 15, 0x00], window.performance.now() + 100);
+    outputs.send([0x90, currentButton + 16, 0x00], window.performance.now() + 100);
+    outputs.send([0x90, currentButton - 16, 0x00], window.performance.now() + 100);
+    outputs.send([0x90, currentButton + 17, 0x00], window.performance.now() + 100);
+    outputs.send([0x90, currentButton - 17, 0x00], window.performance.now() + 100);
+    outputs.send([0x90, currentButton + 1, 0x00], window.performance.now() + 100);
+    outputs.send([0x90, currentButton - 1, 0x00], window.performance.now() + 100);
 
 	}
 }
