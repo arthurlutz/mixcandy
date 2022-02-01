ffd551777fa193745e4097ec61f1a14181c1b304
Arthur Lutz <arthur@lutz.im>
2022-02-01 12:19:33 +0100

    [beatpad.js] updated URL for launchpad pdf

--- js/beatpad.js
+++ js/beatpad.js
@@ -2,7 +2,7 @@
  * beatpad.js
  *
  * (LPR) LaunchPad Programmer's Reference
- * http://d19ulaff0trnck.cloudfront.net/sites/default/files/novation/downloads/4080/launchpad-programmers-reference.pdf
+ * https://d2xhy469pqj8rc.cloudfront.net/sites/default/files/novation/downloads/4080/launchpad-programmers-reference.pdf
  */
 
 (function () {
